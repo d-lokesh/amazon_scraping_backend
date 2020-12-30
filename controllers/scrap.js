40307117c155914a506a343fcd2e612a1552e9ee
lokesh900 <nanibook1@gmail.com>
2020-12-30 17:43:00 +0530

    ff

--- controllers/scrap.js
+++ controllers/scrap.js
@@ -51,12 +51,15 @@ const scrap = async (req, res) => {
   console.log(typeof (req.body.pro), "body")
   for (var x in productslink) {
     c = c + 1;
-    try { await scrapePage(productslink[x], req.body.pro, x) }
-    catch {
-      count = count + 1;
-      console.log("could't get data")
+    // try { }
+    await scrapePage(productslink[x], req.body.pro, x);
 
-    }
+    // catch 
+    // {
+    //   count = count + 1;
+    //   console.log("could't get data");
+
+    // }
 
   }
   console.log("done")
